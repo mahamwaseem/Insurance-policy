169c385d4d29540511316c1a010da496cc7d754f
Maham Waseem <mahamw440@gmail.com>
2025-07-28 19:44:14 +0500

    Created Navbar
    Created files for NAVBAR Header.js and Header.css.
    Change the Font of the title.
    Import Header file in App.js

---
+++ src/My_components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Header.css';
+
+
+export default function Header() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">FinAssure</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/apply">Apply</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/dashboard">DashBoard</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
