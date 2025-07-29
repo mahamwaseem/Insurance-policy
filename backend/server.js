fd596c7fb9d75db52ac0a6b971e011a0b86f345d
Maham Waseem <mahamw440@gmail.com>
2025-07-29 23:55:31 +0500

    Give mongosh URL and port

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,8 @@ app.use(express.json());
 app.use("/api/apply", applicationRoutes);
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect("mongodb://127.0.0.1:27017/loanDB ",{
+
 })
 .then(() => {
   console.log("MongoDB connected");
@@ -27,3 +26,7 @@ mongoose.connect(process.env.MONGO_URI, {
   });
 })
 .catch((err) => console.log(err));
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  });
